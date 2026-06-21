c39a0107e55340804024d6b73eda7912c22101bf
Hamza Osama <hamzaosamaa12@gmail.com>
2026-06-21 18:55:14 +0300

    implemented user and security

---
+++ backend/auth/security.py
@@ -0,0 +1,77 @@
+from argon2 import PasswordHasher
+from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+from uuid import UUID
+from fastapi import HTTPException, status, Depends
+from pydantic import BaseModel
+from datetime import datetime, timedelta, timezone
+import jwt, dotenv, os
+from jwt.exceptions import InvalidTokenError
+from ..models.user import User
+from ..services.dependencies import get_session
+from sqlalchemy.ext.asyncio import AsyncSession
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: UUID | None = None
+
+dotenv.load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY", "")
+ALGORITHM = os.getenv("ALGORITHM", "")
+ACCESS_TOKEN_EXPIRE_MINUTES = float(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 0.0))
+credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+password_context = PasswordHasher()
+
+def hash_password(password: str) -> str:
+    return password_context.hash(password)
+
+def verify_password(hashed_password: str, password: str) -> bool:
+    try:
+        return password_context.verify(hashed_password, password)
+    except:
+        return False
+
+async def Authenticate_user(user: User, password: str):
+    if not verify_password(user.hashed_password, password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    session: AsyncSession = Depends(get_session),
+):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise credentials_exception
+        token_data = TokenData(id=user_id)
+    except InvalidTokenError:
+        raise credentials_exception
+
+    user = await session.get(User, token_data.id)
+
+    if user is None:
+        raise credentials_exception
+    return user
---
+++ backend/models/user.py
@@ -0,0 +1,21 @@
+from sqlmodel import SQLModel, Field
+from enum import Enum
+from uuid import UUID, uuid4
+from datetime import datetime
+from sqlalchemy.orm import declared_attr
+
+class UserRole(str, Enum):
+    admin = "admin"      # full access, manage users
+    analyst = "analyst"  # read/write assets
+    viewer = "viewer"    # read-only
+
+class User(SQLModel, table=True):
+    __tablename__ = 'users' #type: ignore
+    #known issue with SQLModel
+
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    username: str = Field(unique=True, index=True)
+    hashed_password: str
+    role: UserRole = Field(default=UserRole.viewer)
+    org_id: UUID | None = Field(default=None, foreign_key="orgs.id") # links user with an organization
+    created_at: datetime = Field(default_factory=datetime.utcnow) # deprecated but used for factory
