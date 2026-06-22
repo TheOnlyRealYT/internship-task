7eebc0db552760551d525814e4f93fb1e5e67c71
Hamza Osama <hamzaosamaa12@gmail.com>
2026-06-22 22:06:45 +0300

    naming convention update

--- backend/routers/routes_manager.py
+++ backend/routers/routes_manager.py
@@ -7,7 +7,7 @@ from .asset import assetrouter
 
 apirouter = APIRouter()
 apirouter.include_router(authrouter, prefix="/auth", tags=['Auth'])
-apirouter.include_router(userrouter, prefix="/user", tags=['User'])
-apirouter.include_router(orgrouter, prefix="/organization", tags=['Organization'])
+apirouter.include_router(userrouter, prefix="/users", tags=['User'])
+apirouter.include_router(orgrouter, prefix="/organizations", tags=['Organization'])
 apirouter.include_router(router, prefix="/import", tags=['Asset'])
-apirouter.include_router(assetrouter, prefix="/asset", tags=['Asset'])
+apirouter.include_router(assetrouter, prefix="/assets", tags=['Asset'])
