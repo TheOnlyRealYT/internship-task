a49a69b80558b8a7049d339ee3ff8f1c608a1aa5
Hamza Osama <hamzaosamaa12@gmail.com>
2026-06-22 16:22:34 +0300

    added extra endpoints for organizations and included it in the manager

--- backend/routers/organization.py
+++ backend/routers/organization.py
@@ -1,20 +1,24 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, HTTPException, status
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlmodel import select, func
 from ..services.dependencies import get_session
 from ..services.utilities import get_404_error
 from ..models.organization import Organization
+from ..schemas.organization import CreateOrganizationModel
 from ..models.user import UserRole
 from ..auth.security import require_role
 from uuid import UUID
 
 orgrouter = APIRouter()
 
-@orgrouter.get('/organization/{org_id}')
+@orgrouter.get('/{org_id}')
 async def get_organization(org_id: UUID, session: AsyncSession = Depends(get_session)):
-    return await session.get(Organization, org_id)
+    org = await session.get(Organization, org_id)
+    if org is None:
+        raise get_404_error("Organization")
+    return org
 
-@orgrouter.get('/organization/')
+@orgrouter.get('/')
 async def get_many_orgs(
     limit: int = Query(10, ge=0, le=100),
     skip: int = Query(0, ge=0),
@@ -34,11 +38,25 @@ async def get_many_orgs(
         "items": orgs,
     }
 
-@orgrouter.put("/organization/{org_id}", dependencies=[Depends(require_role(UserRole.admin))])
+@orgrouter.patch("/{org_id}", dependencies=[Depends(require_role(UserRole.admin, UserRole.analyst))])
 async def update_org(org_id: UUID, name: str, session: AsyncSession = Depends(get_session)):
     org = await session.get(Organization, org_id)
     if org is None : raise get_404_error("Organization")
     org.name = name
     session.add(org)
+    await session.commit()
+    return org
+
+@orgrouter.post("/", dependencies=[Depends(require_role(UserRole.admin, UserRole.analyst))])
+async def create_org(org: CreateOrganizationModel, session: AsyncSession = Depends(get_session)):
+    new_org = Organization(name = org.name)
+    session.add(new_org)
+    await session.commit()
+    return new_org
+
+@orgrouter.delete("/{org_id}", dependencies=[Depends(require_role(UserRole.admin, UserRole.analyst))])
+async def delete_org(org_id: UUID, session: AsyncSession = Depends(get_session)):
+    org = await session.get(Organization, org_id)
+    await session.delete(org)
     await session.commit()
     return org
--- backend/routers/routes_manager.py
+++ backend/routers/routes_manager.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from .auth import authrouter
 from .user import userrouter
+from .organization import orgrouter
 
 apirouter = APIRouter()
 apirouter.include_router(authrouter, prefix="/auth", tags=['Auth'])
 apirouter.include_router(userrouter, prefix="/user", tags=['User'])
+apirouter.include_router(orgrouter, prefix="/organization", tags=['Organization'])
